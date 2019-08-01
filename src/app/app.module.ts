0a137e410b89bafd88d95941a1cc657b011e5867
Erick Tepale <e.tepale@gmail.com>
2019-08-01 00:30:38 -0400

    all landing page is done idk if deploy or work on next page ?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './component/landing-page/landing-page.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
